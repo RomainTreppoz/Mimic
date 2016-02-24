d891bcc6532566ddf18d164bc9a3dcb22d633bc1
RomainTreppoz <romaintreppoz@gmail.com>
2016-02-24 15:49:30 +0100

    index et login OK. Inscription en bdd OK + Harmoniser

--- app/templates/register/index.php
+++ app/templates/register/index.php
@@ -15,58 +15,34 @@
        
         <div class="col-md-6">
 
+        <form class="form-container" method="POST" action="<?= $this->url('registerUser'); ?>"> 
+          <div class="form-group <?php if(isset($errors['email'])) echo 'has-error' ?>">
 
-<div class="col-md-6 col-md-offset-3">
+            <label for="email">Adresse électronique</label>
+            <input type="text" class="form-control" id="email" name="email" value="<?php if(isset($email)) echo $email; ?>" placeholder="Email">
+            <?php if(isset($errors['email'])): ?>
+              <span class="help-block"><?= $errors['email']; ?></span>
+            <?php endif; ?>
+            <?php if(isset($errors['password'])): ?>
+              <span class="help-block"><?= $errors['password']; ?></span>
+            <?php endif; ?>
+          </div>
 
-    <form class="form-container" method="POST" action="<?= $this->url('registerUser'); ?>">
-      <div class="form-group <?php if(isset($errors['email'])) echo 'has-error' ?>">
-        <label for="email">Email :</label>
-        <input type="email" class="form-control" id="email" name="email" value="<?php if(isset($email)) echo $email; ?>" placeholder="Email">
-        <?php if(isset($errors['email'])): ?>
-          <span class="help-block"><?= $errors['email']; ?></span>
-        <?php endif; ?>
-      </div>
+          <div class="form-group <?php if(isset($errors['password'])) echo 'has-error' ?>">
+            <label for="password">Mot de passe</label>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+          </div>
 
-      <div class="form-group <?php if(isset($errors['password'])) echo 'has-error' ?>">
-        <label for="password">Password :</label>
-        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-        <?php if(isset($errors['password'])): ?>
-          <span class="help-block"><?= $errors['password']; ?></span>
-        <?php endif; ?>
-      </div>
+          <div class="form-group">
+            <label for="passwordConfirm">Confirmer le mot de passe</label>
+            <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" placeholder="Password">
+          </div>
 
-      <div class="form-group">
-        <label for="confirmPassword">Confirm Password :</label>
-        <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" placeholder="Password">
-      </div>
+          <button type="submit" class="btn btn-warning btn-lg" id="btnEnvoyer" name="action">
+            <span class="glyphicon glyphicon-log-in"  aria-hidden="true"></span>   OK
+          </button>
 
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-
-  </div>        
-
-        
-         
-            <div class="form-group">
-                    <label for="email">Adresse électronique</label>
-                    <input type="text" class="form-control" id="email" name="email" placeholder="Email">
-                  </div>
-
-                  <div class="form-group">
-                    <label for="password">Mot de passe</label>
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-                  </div>
-
-                  <div class="form-group">
-                    <label for="passwordConfirm">Confirmer le mot de passe</label>
-                    <input type="password" class="form-control" id="passwordConfirm" name="passwordConfirm" placeholder="Confirm Password">
-                  </div>
-
-                  <button type="submit" class="btn btn-warning btn-lg" id="btnEnvoyer" name="action">
-                    <span class="glyphicon glyphicon-log-in"  aria-hidden="true"></span>   OK
-                  </button>
-
-        
+          </form>
         </div> <!-- div col-md-6 -->
 
       </div> <!-- col-md-12 -->
