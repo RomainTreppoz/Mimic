7128c462e87ff3a8335f33536b618e1b3a24eaf0
RomainTreppoz <romaintreppoz@gmail.com>
2016-02-25 10:36:00 +0100

    customisation erreur 403

--- app/templates/w_errors/403.php
+++ app/templates/w_errors/403.php
@@ -1,5 +1,6 @@
 <?php $this->layout('layout', ['title' => 'Créer un nouvel administrateur']) ?>
 
 <?php $this->start('main_content'); ?>
-<h1>403. Nothing to see here.</h1>
+<h1>403. Rien à voir par ici.</h1>
+<img src="<?= $this->assetUrl("img/016_doute.jpg")?>"class="mimic img-responsive" alt="erreur 403">
 <?php $this->stop('main_content'); ?>
--- app/templates/w_errors/404.php
+++ app/templates/w_errors/404.php
@@ -1,6 +1,6 @@
 <?php $this->layout('layout', ['title' => 'Perdu ?']) ?>
 
 <?php $this->start('main_content'); ?>
-<h1>Zut il n'y a rien ici.</h1>
+<h1>404. Zut il n'y a rien ici.</h1>
 <img src="<?= $this->assetUrl("img/404.jpg")?>"class="mimic img-responsive" alt="erreur 404">
 <?php $this->stop('main_content'); ?>
