25de3f2a485b14b9a04cbd0e31972a16b7948616
RomainTreppoz <romaintreppoz@gmail.com>
2016-02-25 15:41:08 +0100

    prend les photos les pose dans upload

--- app/Controller/PublishController.php
+++ app/Controller/PublishController.php
@@ -25,22 +25,48 @@ class PublishController extends Controller
 		$image2 = htmlentities(trim($_POST['snapPhotoData-2']));
 		$image3 = htmlentities(trim($_POST['snapPhotoData-3']));
 
-echo $image3;
-
-die;
 		$errors=[];
-		if (!empty($image1) || !empty($image2) || !empty($image3)) {
+
+		if (empty($image1) || empty($image2) || empty($image3)) {
 			$errors['image']="Toutes les images sont obligatoires";
+			$this->show('publish/index', ['errors' => $errors]);
 		}
 
 		if (empty($errors)) {
+			/* Image1 */
+			list($type, $data) = explode(';', $image1);
+			list(, $data)      = explode(',', $data);
+			$data = base64_decode($data);
+
+				/* On génère un nom aléatoire pour l'image 1 */
+				$tmpfname1 = tempnam(__DIR__.'/../../uploads/', "");
+				file_put_contents($tmpfname1.'.png', $data);
+
+			/* Image2 */
+
+			list($type, $data) = explode(';', $image2);
+			list(, $data)      = explode(',', $data);
+			$data = base64_decode($data);
+
+				/* On génère un nom aléatoire pour l'image 2 */
+				$tmpfname2 = tempnam(__DIR__.'/../../uploads/', "");
+				file_put_contents($tmpfname2.'.png', $data);
+
+			/* Image3 */
+			list($type, $data) = explode(';', $image3);
+			list(, $data)      = explode(',', $data);
+			$data = base64_decode($data);
+
+				/* On génère un nom aléatoire pour l'image 3 */
+				$tmpfname3 = tempnam(__DIR__.'/../../uploads/', "");
+				file_put_contents($tmpfname3.'.png', $data);			
+
+
 			// j'insère en bdd s'il n'y a pas d'erreur
+			$_SESSION['message']="Votre stip a bien été enregistré";
 			$this->redirectToRoute('home');
 		}
-		else {
-			$this->show('publish/index', ['errors' => $errors]);
-
-		}
+		
 	}
 
 }
--- app/templates/layout.php
+++ app/templates/layout.php
@@ -84,7 +84,13 @@
   <div id="main">           
     <div class="container-fluid">
 
-	    <!-- Contenu dynamique -->
+	    
+    <?php if(isset($_SESSION['message'])) :?>
+      <?= $_SESSION['message']; ?>
+      <?php unset($_SESSION['message']); ?>
+    <?php endif; ?> 
+      <!-- Contenu dynamique -->
+
 		<?= $this->section('main_content') ?>
 
 
